83906d1a46ce59aabb8fca68abd2cf8ca964d6f8
Fleudry <chreydmapeba@gmail.com>
2023-04-16 02:33:23 +0000

    Autorisation avec les Gates

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class TestController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except([
+        /* $this->middleware('auth')->except([
             'bar',
-        ]);
+        ]); */
+        $this->middleware('auth');
     }
     public function foo()
     {
+        if (Gate::allows('access-admin')) {
+            abort('403');
+        }
+
         return view('test.foo');
     }
     public function bar()
